187953e5dc3bd51502b359de0f939e875508d673
Julisa <julisa.leon344@gmail.com>
2026-01-25 13:47:04 -0500

    fix de type

--- types/aos.d.ts
+++ types/aos.d.ts
@@ -1,1 +1,13 @@
-declare module 'aos';
+declare module "aos" {
+  interface AOSOptions {
+    duration?: number
+    once?: boolean
+    offset?: number
+    easing?: string
+  }
+  interface AOS {
+    init(options?: AOSOptions): void
+  }
+  const aos: AOS
+  export default aos
+}
